40f2d10d262d6d3c13aae7cd9261e0755f7e27df
MasonPM <masonpmilligan@gmail.com>
2021-03-07 11:26:51 -0700

    Added Raspberry Pi support, GPS, OLED

---
+++ driver.py
@@ -0,0 +1,16 @@
+from oled import oled_setup, display_plot
+from read_gps import get_coords
+from tracker import openskyAPICurrStatus, plotPlanes
+
+
+def run():
+    oled_setup()
+    coords = get_coords()
+    dictionary = openskyAPICurrStatus(coords.lat, coords.lon)
+    coord_list = plotPlanes(coords.lat, coords.lon, dictionary)
+    # print(coord_list)
+    display_plot(coord_list)
+
+
+if __name__ == '__main__':
+    run()
